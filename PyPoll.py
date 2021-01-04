6a7dc47f725290db52f5d43765f715cf2ad5514a
christybell <bell.christy27@gmail.com>
2021-01-03 19:29:40 -0600

    added list of candidates

--- PyPoll.py
+++ PyPoll.py
@@ -1,5 +1,64 @@
+# Add our dependencies.
 import csv
 import os
+
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path. 
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read the header row.
+    headers = next(file_reader)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        # total_votes +=1
+        total_votes = total_votes + 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the candidate list.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list
+print(candidate_options)
+
+# # Print the total votes.
+# print(total_votes)
+
+    # for total_votes in election_data:
+    #     total_votes = total_votes + 1
+
+    #     print(total_votes)
+
+    # # Print each row in the CSV file.
+    # for row in file_reader:
+    #     print(row)
+
+    # for vote in election_data:
+    #     votecounter = votecounter + 1
+       
+    #print(election_data)
+
+# # Close the file.
+# election_data.close()
+
+# print(votecounter)
 
 # The data we need to retrieve.
 # 1. The total number of votes cast
@@ -8,31 +67,8 @@ import os
 # 4. The total number of votes each candidate won
 # 5. The winner of the election base don popular vote.
 
-# Assign a variable fo the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
 
-votecounter = 0
-
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
-    columnnames = next(election_data)
-
-    # To do: perform analysis.
-    for vote in election_data:
-        votecounter = votecounter + 1
-       
-    #print(election_data)
-
-# Close the file.
-election_data.close()
-
-print(votecounter)
-
-
-# # Assign a variable for the file to load and the path.
-# file_to_load = os.path.join("Resources", "election_results.csv")
-# # Open the election results and read the file.
-# with open(file_to_load) as election_data:
-
-#     # Print the file object.
-#     print(election_data)
+#     #Write three counties to the file.
+#     txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
